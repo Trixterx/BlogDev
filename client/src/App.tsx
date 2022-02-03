b0d3739e684b426bce353f0ddb01730637ec3c39
Dennis Lindquist <dennislindquist@gmail.com>
2022-02-03 19:58:57 +0100

    setup client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
-function App() {
+import PageRender from './PageRender'
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <div className="container">
+    <Router>
+      <Routes>
+        <Route path="/" element={<PageRender />} />
+        <Route path="/:page" element={<PageRender />} />
+        <Route path="/:page/:slug" element={<PageRender />} />
+      </Routes>
+    </Router>
+  </div>
+  )
+};
 
-export default App;
+export default App
