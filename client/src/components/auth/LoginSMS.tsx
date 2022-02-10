5b463afb1ec15f748df9929d288247b2d590ddf0
Dennis Lindquist <dennislindquist@gmail.com>
2022-02-10 09:49:13 +0100

    login & auth done

--- client/src/components/auth/LoginSMS.tsx
+++ client/src/components/auth/LoginSMS.tsx
@@ -4,10 +4,14 @@ const LoginSMS = () => {
     const [phone, setPhone] = useState('')
   return (
   <form>
-      <div className="form_group">
+      <div className="form_group mb-3">
           <label htmlFor="phone" className="form-label">Phone number</label>
           <input type="text" className='form-control' id="phone" value={phone} onChange={e => setPhone(e.target.value)}></input>
       </div>
+
+      <button type="submit" className='btn btn-dark w-100' disabled={phone ? false : true}>
+          Login
+      </button>
   </form>
   );
 };
